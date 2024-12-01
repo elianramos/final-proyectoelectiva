8815da301072268347460697b41d2d755b494104
elianramos <elianramosacosta@gmail.com>
2024-11-30 22:20:19 -0400

    Add project files.

---
+++ LoadDWv/LoadDWv.Data/Context/NWContext.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace LoadDWv.Data.Context
+{
+    public partial class NWContext : DbContext
+
+    { public NWContext(DbContextOptions<NWContext> options) base(options)
+    }
+}
---
+++ LoadDWv/LoadDWv.WorkerService/Program.cs
@@ -0,0 +1,7 @@
+using LoadDWv.WorkerService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
